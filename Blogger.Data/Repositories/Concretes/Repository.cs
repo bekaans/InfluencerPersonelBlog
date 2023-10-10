04fd83f65c94440749c22942406c543e891ff72f
Berke Kaan <bekaans@icloud.com>
2023-10-10 16:43:34 +0300

    .gitignoew

---
+++ Blogger.Data/Repositories/Concretes/Repository.cs
@@ -0,0 +1,17 @@
+﻿using Blogger.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.Data.Repositories.Concretes
+{
+    public class Repository
+    {
+        public Repository(AppDbContext dbContext)
+        {
+
+        }
+    }
+}
