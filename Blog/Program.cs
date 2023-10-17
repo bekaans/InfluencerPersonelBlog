83d2aa05e21928c4abf72a8a1296d30169fc9c49
Berke Kaan <bekaans@icloud.com>
2023-10-17 13:05:19 +0300

    Add Builder

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,10 +1,12 @@
 using Blogger.Data.Context;
 using Blogger.Data.Extensions;
+using Blogger.Service.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.LoadDataLayerExtensions(builder.Configuration);
+builder.Services.LoadServiceLayerExtensions();
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
