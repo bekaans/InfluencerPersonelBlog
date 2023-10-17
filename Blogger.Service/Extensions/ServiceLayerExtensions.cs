85fea420b46fcb35559c70cab0481246f438816b
Berke Kaan <bekaans@icloud.com>
2023-10-17 13:04:42 +0300

    Add ServiceLayerExtensions => Service/Extensions/

---
+++ Blogger.Service/Extensions/ServiceLayerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Blogger.Data.Context;
+using Blogger.Data.Repositories.Abstraction;
+using Blogger.Data.Repositories.Concretes;
+using Blogger.Data.UnitOfWorks;
+using Blogger.Service.Services.Abstraction;
+using Blogger.Service.Services.Concretes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.Service.Extensions
+{
+    public static class ServiceLayerExtensions
+    {
+        public static IServiceCollection LoadServiceLayerExtensions(this IServiceCollection services)
+        {
+            services.AddScoped<IArticleService, ArticleService>();
+            return services;
+        }
+    }
+}
