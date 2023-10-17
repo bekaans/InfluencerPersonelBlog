081d8559af2b3f19275fcd186791e328d89f89c6
Berke Kaan <bekaans@icloud.com>
2023-10-17 13:04:05 +0300

    Add ArticleService,IArticleService => Services/Concretes,Abstraction/

---
+++ Blogger.Service/Services/Abstraction/IArticleService.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.Service.Services.Abstraction
+{
+    public interface IArticleService
+    {
+        Task<List<Article>> GetAllArticle();
+
+    }
+}
---
+++ Blogger.Service/Services/Concretes/ArticleService.cs
@@ -0,0 +1,25 @@
+﻿using Blog.Entity.Entities;
+using Blogger.Data.UnitOfWorks;
+using Blogger.Service.Services.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.Service.Services.Concretes
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public ArticleService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<Article>> GetAllArticle()
+        {
+            return await unitOfWork.GetRepository<Article>().GetAllAsync(); 
+        }
+    }
+}
