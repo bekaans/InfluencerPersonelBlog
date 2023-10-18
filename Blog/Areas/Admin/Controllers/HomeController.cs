b3a7ada855d24c54435bc604a9803c8e96359484
Berke Kaan <bekaans@icloud.com>
2023-10-18 12:38:48 +0300

    Add HomeController => /Admin/Controllers/

---
+++ Blog/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Blogger.Service.Services.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogger.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly IArticleService articleService;
+
+        public HomeController(IArticleService articleService)
+        {
+            this.articleService = articleService;
+        }
+
+        public async Task<IActionResult> Index()
+        { 
+            var articles=await articleService.GetAllArticlesAsync();
+            return View(articles);
+        }
+    }
+}
