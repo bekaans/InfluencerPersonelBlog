8711b0b868e914cf5848e7e70561f82e2d38c14d
Berke Kaan <bekaans@icloud.com>
2023-10-19 20:55:12 +0300

    Add ArticleProfile => Service/AutoMapper/Articles/

---
+++ Blogger.Service/AutoMapper/Articles/ArticleProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Blog.Entity.DTOs.Articles;
+using Blog.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.Service.AutoMapper.Articles
+{
+    public class ArticleProfile : Profile
+    {
+        protected ArticleProfile()
+        {
+            CreateMap<ArticleDTO, Article>().ReverseMap();
+        }
+    }
+}
